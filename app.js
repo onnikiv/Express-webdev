9030850983e89b1aef1c85c66b2b86160d74ee6b
onnik <onni.kivinen@gmail.com>
2025-04-01 14:36:21 +0300

    Update welcome message in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my EXPRESS BOII!');
+  res.send('Here will the my Express assignments :)');
 });
 
 app.listen(port, hostname, () => {
