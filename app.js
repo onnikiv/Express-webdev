ed8a45474dac684e0e080a4b31bd41d38b3f8dd3
onnik <onni.kivinen@gmail.com>
2025-04-02 13:40:20 +0300

    Add static file serving and API endpoint for cat information

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3001;
 
+app.use('/public', express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Here will the my Express assignments :)');
+});
+
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    cat_id: 246,
+    name: 'Kerttu-poika',
+    birthdate: '2006-11-05',
+    weight: 8,
+    owner: 'ile',
+    image: 'https://loremflickr.com/320/240/cat',
+  };
+  res.json(cat);
 });
 
 app.listen(port, hostname, () => {
