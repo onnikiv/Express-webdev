a689aa7c4376309d571c20e7e17cc2f0b6b14528
onnik <onni.kivinen@gmail.com>
2025-04-02 13:41:06 +0300

    Update cat information in API response

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/v1/cat', (req, res) => {
   const cat = {
-    cat_id: 246,
-    name: 'Kerttu-poika',
-    birthdate: '2006-11-05',
-    weight: 8,
-    owner: 'ile',
+    cat_id: 1,
+    name: 'Jorma',
+    birthdate: '1963-12-01',
+    weight: 30,
+    owner: 'onni',
     image: 'https://loremflickr.com/320/240/cat',
   };
   res.json(cat);
