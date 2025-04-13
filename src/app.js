638e86ab7621ea23e12c3b3177d83bec91a8f451
onnik <onni.kivinen@gmail.com>
2025-04-13 13:21:59 +0300

    Enable CORS support in the Express application

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors'; // Import the cors package
 import api from './api/index.js';
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
