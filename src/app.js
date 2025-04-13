d1d0eb087b5c308a3276a39792bdf6e0b254b087
onnik <onni.kivinen@gmail.com>
2025-04-13 13:25:33 +0300

    lisätty cors origin käyttämään vain omia osotteita

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import cors from 'cors'; // Import the cors package
+import cors from 'cors';
 import api from './api/index.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
